1951b79358b3507450bed12ce9f0285cd5b88434
Robby <buckdestructor@gmail.com>
2022-10-19 21:24:47 -0400

    Homepage buttons working

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,57 +2,57 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import {useCollection} from "react-firebase-hooks/firestore"
+import { useCollection } from "react-firebase-hooks/firestore"
 import { getFirestore, collection, query, where, getDocs } from "firebase/firestore";
 import { useEffect } from 'react'
 import { db } from "../firebase/firestore";
 
 const Home: NextPage = () => {
 
-  const [value, loading, error] = useCollection(
-    collection(db, 'Users'),
-    {
-      snapshotListenOptions: { includeMetadataChanges: true },
-    }
-  );
+    const [value, loading, error] = useCollection(
+        collection(db, 'Users'),
+        {
+            snapshotListenOptions: { includeMetadataChanges: true },
+        }
+    );
 
-  
 
-  return (
-    <div >
-      <Head>
-        <title>Chord Explorer</title>
-        <meta name="description" content="A music education website where you can search for songs based on the chords you're learning/teaching, as well as getting community based feedback on your playing!" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
 
-      <main className='grid grid-cols-3 '>
-        <div className='row-start-3 p-4 bg-[#120724] ml-16 mt-80'>
-        <h2 className='p-2 text-4xl'>
-          Learn guitar, without the boring stuff! 
-        </h2>
+    return (
+        <div >
+            <Head>
+                <title>Chord Explorer</title>
+                <meta name="description" content="A music education website where you can search for songs based on the chords you're learning/teaching, as well as getting community based feedback on your playing!" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
 
-        <p className='p-2'>
-          Learn to play chords on the guitar while simultaneously learning the songs you know and love.
-        </p>
+            <main className='grid grid-cols-3 '>
+                <div className='row-start-3 p-4 bg-[#120724] ml-16 mt-80'>
+                    <h2 className='p-2 text-4xl'>
+                        Learn guitar, without the boring stuff!
+                    </h2>
 
-        <p className='p-2'>
-          Search for songs to learn based on what chords you are learning, then submit your performance for community based feedback.
-        </p>
+                    <p className='p-2'>
+                        Learn to play chords on the guitar while simultaneously learning the songs you know and love.
+                    </p>
 
-        <div className='flex flex-row gap-4 p-4 pl-2 text-xl font-medium '>
-          <a href="https://github.com/robbyph/chord-explorer" className='px-4 py-2 bg-[#5B21B6] rounded-md col-span-2'>
-            Search
-          </a>
-          <a href="" className='px-4 py-2 bg-[#5B21B6] rounded-md col-span-3'>
-            Get Feedback
-          </a>
+                    <p className='p-2'>
+                        Search for songs to learn based on what chords you are learning, then submit your performance for community based feedback.
+                    </p>
+
+                    <div className='flex flex-row gap-4 p-4 pl-2 text-xl font-medium '>
+                        <a href="/search" className='px-4 py-2 bg-[#5B21B6] rounded-md col-span-2'>
+                            Search
+                        </a>
+                        <a href="/submitfeedback" className='px-4 py-2 bg-[#5B21B6] rounded-md col-span-3'>
+                            Get Feedback
+                        </a>
+                    </div>
+                </div>
+
+            </main>
         </div>
-        </div>
-        
-      </main>
-    </div>
-  )
+    )
 }
 
 export default Home
