82aa5cd957dfcef7afa72c0478667006be6f86f0
Andrew Glanz <andrewvita0@gmail.com>
2022-10-24 16:02:33 -0400

    adding a bit of styling

--- pages/chordlibrary.tsx
+++ pages/chordlibrary.tsx
@@ -11,14 +11,13 @@ const chordlibrary = () => {
         </Head>
         <main> 
           <h1 className='col-span-4 p-6 text-4xl'>Chord Library</h1>
-          <p className='text-sm'>Chord Root</p>
-          <select id="chordDropdown">
+          <p className='text-sm p-6'>Chord Root</p>
+          <select className='p=6' id="chordDropdown">
             <option disabled selected> E, A, C#, etc.</option>
             <option>Option 1</option>
             <option>Option 2</option>
             <option>Option 3</option>
           </select>
-          
         </main>
     </div>
   )
