62760b67f3f2f7b203081fe6eb11b38e2bb1048d
Andrew Glanz <andrewvita0@gmail.com>
2022-10-24 15:58:46 -0400

    working on fleshing out chordlibrary.tsx

--- pages/chordlibrary.tsx
+++ pages/chordlibrary.tsx
@@ -9,9 +9,16 @@ const chordlibrary = () => {
             <meta name="description" content="A music education website where you can search for songs based on the chords you're learning/teaching, as well as getting community based feedback on your playing!" />
             <link rel="icon" href="/favicon.ico" />
         </Head>
-        <main > 
+        <main> 
           <h1 className='col-span-4 p-6 text-4xl'>Chord Library</h1>
-          <p>Chord Root</p>
+          <p className='text-sm'>Chord Root</p>
+          <select id="chordDropdown">
+            <option disabled selected> E, A, C#, etc.</option>
+            <option>Option 1</option>
+            <option>Option 2</option>
+            <option>Option 3</option>
+          </select>
+          
         </main>
     </div>
   )
