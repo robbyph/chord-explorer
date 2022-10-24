af8a9f69f1a34d6888d25f781f4d7704b566687a
Andrew Glanz <andrewvita0@gmail.com>
2022-10-24 16:15:54 -0400

    formatted pages using prettier autoformatter

--- pages/search.tsx
+++ pages/search.tsx
@@ -1,19 +1,22 @@
-import React from 'react'
-import Head from 'next/head'
+import React from "react";
+import Head from "next/head";
 
 const search = () => {
   return (
     <div>
       <Head>
-            <title>Chord Explorer</title>
-            <meta name="description" content="A music education website where you can search for songs based on the chords you're learning/teaching, as well as getting community based feedback on your playing!" />
-            <link rel="icon" href="/favicon.ico" />
-        </Head>
-        <main > 
-          <h1 className='col-span-4 p-6 text-4xl'>Search</h1>
-        </main>
+        <title>Chord Explorer</title>
+        <meta
+          name="description"
+          content="A music education website where you can search for songs based on the chords you're learning/teaching, as well as getting community based feedback on your playing!"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <h1 className="col-span-4 p-6 text-4xl">Search</h1>
+      </main>
     </div>
-  )
-}
+  );
+};
 
-export default search
+export default search;
