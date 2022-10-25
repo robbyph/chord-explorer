84dcbbcc5e93624571432beda5b49c36771e31a5
Robby <buckdestructor@gmail.com>
2022-10-25 13:06:48 -0400

    Implemented Header

---
+++ components/Header.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Header = () => {
+  return <span className='text-4xl'>Chord Explorer</span>;
+};
+
+export default Header;
--- components/Layout.js
+++ components/Layout.js
@@ -1,11 +1,13 @@
 import Navigation from '../components/Navigation';
+import Header from '../components/Header';
 
 const Layout = ({ children }) => {
   return (
     <>
       <meta content='width=device-width, initial-scale=1' name='viewport' />
       <div>
-        <div>
+        <div className='flex flex-row p-2 text-black bg-white'>
+          <Header></Header>
           <Navigation></Navigation>
         </div>
         <main>{children}</main>
