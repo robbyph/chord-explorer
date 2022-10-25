70001c245c2de498e5012e7a44b2e429dc8db1c2
Robby <buckdestructor@gmail.com>
2022-10-25 14:46:13 -0400

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -4,7 +4,7 @@ import Link from 'next/link';
 const Header = () => {
   return (
     <Link href='/'>
-      <a href='/' className='z-10 pr-8 font-semibold text-black pl-28'>
+      <a className='z-10 pr-8 font-semibold text-black pl-28'>
         <span className='text-4xl leading-5'>Chord Explorer</span>
       </a>
     </Link>
