11048ca45bb6730b533087236b097672f92564ed
Robby <buckdestructor@gmail.com>
2022-10-25 13:08:25 -0400

    Made header link home

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import Link from 'next/link';
 
 const Header = () => {
-  return <span className='text-4xl'>Chord Explorer</span>;
+  return (
+    <Link href='/'>
+      <a href='/'>
+        <span className='text-4xl'>Chord Explorer</span>
+      </a>
+    </Link>
+  );
 };
 
 export default Header;
