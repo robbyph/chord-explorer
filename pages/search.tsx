36ccb607813749dd2f4e6029b5532b9d03ef72ae
Robby <buckdestructor@gmail.com>
2022-10-31 22:50:59 -0400

    Added combo boxes to search

--- pages/search.tsx
+++ pages/search.tsx
@@ -14,6 +14,46 @@ const search = () => {
       </Head>
       <main>
         <h1 className="col-span-4 p-6 text-4xl">Search</h1>
+        <div className="flex flex-row">
+            <div className="m-6">
+                <p className="font-bold text-md">Chord Root</p>
+                <select className="mt-2 text-md" id="chordDropdown">
+                  <option value='ab'>
+                    Ab/G# 
+                  </option>
+                  <option value='a'>
+                    A
+                  </option>
+                  <option value='bb'>
+                    Bb/A#
+                  </option>
+                  <option value='b'>
+                    B
+                  </option>
+                  <option value='c'>C</option>
+                  <option value='db'>
+                    Db/C#
+                  </option>
+                  <option value='d'>D</option>
+                  <option value='eb'>Eb/D#</option>
+                  <option value='e'>E</option>
+                  <option value='f'>F</option>
+                  <option value='gb'>F#/Gb</option>
+                  <option value='g'>G</option>
+                </select>
+            </div>
+            <div className="m-6">
+                <p className="font-bold text-md">Chord Quality</p>
+                <select className="mt-2 text-md" id="chordDropdown">
+                  <option value='major'>
+                    Major 
+                  </option>
+                  <option value='minor'>
+                    Minor
+                  </option>
+                </select>
+            </div>
+        </div>
       </main>
     </div>
   );
