1d390709f09bcf95819d07c3435932e6d821362e
Robby <buckdestructor@gmail.com>
2022-11-14 01:03:57 -0500

    Got sound almost loading
    Only issue is that i can't get the buffer to not be empty

--- pages/chordrecognition.tsx
+++ pages/chordrecognition.tsx
@@ -1,12 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Head from "next/head";
 import { detectChords } from "akkorder/src";
 import { C_Major_48 } from "akkorder/__tests__/testSignals";
 
 const chordrecognition = () => {
 
-    let chords = detectChords(C_Major_48, 44100);
-    console.log(chords)
+    var sourceBuffer: AudioBuffer;
+    var context: AudioContext;
+
+    useEffect(() => {
+        context = new AudioContext();
+    })
+
+    function loadSound(url: string | URL) {
+        context.resume().then(() => {
+            console.log('resumed')
+            var request = new XMLHttpRequest();
+            request.open('GET', url, true);
+            request.responseType = 'arraybuffer';
+            // Decode asynchronously
+            request.onload = function () {
+                console.log('request')
+                console.log(request.response);
+                context.decodeAudioData(request.response);
+                console.log('audio context')
+                console.log(context)
+                console.log('buffer')
+                console.log(sourceBuffer)
+            }
+            request.send();
+
+        })
+    }
+
 
     return (
         <div>
@@ -21,6 +47,7 @@ const chordrecognition = () => {
             <main>
                 <h1 className="col-span-4 p-6 text-4xl">Chord Recognition</h1>
                 <h2 className="col-span-4 p-6 text-4x1">Powered by AI</h2>
+                <button onClick={() => { loadSound('example.mp3') }}>Detect</button>
             </main>
         </div>
     );
