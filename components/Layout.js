67f20f95c13d2428ed06082548efb91fca451ba0
Andrew Glanz <andrewvita0@gmail.com>
2023-02-15 20:25:02 -0500

    cool navbar display for smaller devices.

--- components/Layout.js
+++ components/Layout.js
@@ -6,7 +6,11 @@ const Layout = ({ children }) => {
     <>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <div>
-        <div className="min-[60px]:max-[1400px]:hidden xl:visible flex items-center pb-14 pt-6 flex-row relative h-fit w-full before:p-0 before:m-0 overflow-hidden before:rounded-[100%] before:absolute before:bg-white before:top-[-550px] before:bottom-0 before:left-[-40%] before:right-[-20%]">
+        <div className="min-[60px]:max-[1450px]:hidden min-[1451px]:visible flex items-center pb-14 pt-6 flex-row relative h-fit w-full before:p-0 before:m-0 overflow-hidden before:rounded-[100%] before:absolute before:bg-white before:top-[-550px] before:bottom-0 before:left-[-40%] before:right-[-20%]">
+          <Header></Header>
+          <Navigation></Navigation>
+        </div>
+        <div className="min-[60px]:max-[1450px]:visible min-[1451px]:hidden flex items-center pb-6 pt-6 flex-row relative h-fit w-full before:p-0 before:m-0 overflow-hidden before:absolute before:bg-white before:top-[-550px] before:bottom-0 before:left-[-40%] before:right-[-20%]">
           <Header></Header>
           <Navigation></Navigation>
         </div>
