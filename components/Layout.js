b91d0bb288c1421fc9b539df48873e86555bd5a6
Andrew Glanz <andrewvita0@gmail.com>
2023-02-15 20:13:09 -0500

    changed modifiers to make room for new nav

--- components/Layout.js
+++ components/Layout.js
@@ -6,7 +6,7 @@ const Layout = ({ children }) => {
     <>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <div>
-        <div className="min-[60px]:max-[1400px]:invisible xl:visible flex items-center pb-14 pt-6 flex-row relative h-fit w-full before:p-0 before:m-0 overflow-hidden before:rounded-[100%] before:absolute before:bg-white before:top-[-550px] before:bottom-0 before:left-[-40%] before:right-[-20%]">
+        <div className="min-[60px]:max-[1400px]:hidden xl:visible flex items-center pb-14 pt-6 flex-row relative h-fit w-full before:p-0 before:m-0 overflow-hidden before:rounded-[100%] before:absolute before:bg-white before:top-[-550px] before:bottom-0 before:left-[-40%] before:right-[-20%]">
           <Header></Header>
           <Navigation></Navigation>
         </div>
