0eb4cb7b4feb3b32ab652a2ae9d3293a4cc2bb54
Robby <buckdestructor@gmail.com>
2023-02-16 01:17:04 -0500

    Changed some alert styling

--- components/SignInPrompt.js
+++ components/SignInPrompt.js
@@ -8,7 +8,7 @@ const SignInPrompt = ({ setShow }) => {
 
   return (
     <div
-      className={`text-black font-bold xl:left-1/3 bg-purple-200 mt-4 xl:ml-18 top-0 z-10 fixed animate-slideup font-IBMPlexSans px-2 pb-4 shadow-md shadow-neutral-800`}
+      className={`text-black  xl:left-1/3 bg-purple-200 mt-4 xl:ml-18 top-0 z-10 fixed animate-slideup font-IBMPlexSans px-2 pb-4 shadow-md shadow-neutral-800`}
       css={{
         bottom: 0,
         left: 0,
@@ -17,14 +17,16 @@ const SignInPrompt = ({ setShow }) => {
     >
       <div className='flex items-start justify-between'>
         <div className='flex flex-col justify-center text-center'>
-          <span className='px-10 pt-6 text-2xl text-center '>
+          <span className='px-10 pt-6 text-2xl font-semibold text-center '>
             You need an account to do that.
           </span>{' '}
           <span className='px-10 pt-6 text-base '>
             Wanna join the community?
           </span>{' '}
           <Link href='/signup'>
-            <a className='p-4 pt-0 pb-2 text-center underline'>Sign Up Here!</a>
+            <a className='p-4 pt-0 pb-2 text-center underline'>
+              <em>Sign Up Here!</em>
+            </a>
           </Link>
         </div>
         <button onClick={handleClose} className='px-2 py-1 font-medium'>
