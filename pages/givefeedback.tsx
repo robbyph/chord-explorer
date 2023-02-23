4cfa3cc3161a5825ae0e1e609acf96e6a01b849c
Robby <buckdestructor@gmail.com>
2023-02-23 16:04:42 -0500

    Made some more progress on getting giveFeedback to showup with the account name, no luck so far

--- pages/givefeedback.tsx
+++ pages/givefeedback.tsx
@@ -2,7 +2,7 @@
 
 import React, { useEffect, useState } from "react";
 import Head from "next/head";
-import { collection, getDocs, query, orderBy } from "firebase/firestore";
+import { collection, getDocs, query, orderBy, doc, getDoc } from "firebase/firestore";
 import { db } from "../firebase/firestore";
 import { NextPage } from 'next';
 import { useCollection } from "react-firebase-hooks/firestore";
@@ -10,21 +10,12 @@ import Link from "next/link";
 
 
 const GiveFeedback = () => {
-
     const [posts, loading, error] = useCollection(
         query(collection(db, 'Posts'), orderBy('created', 'desc')),
         {
             snapshotListenOptions: { includeMetadataChanges: true },
         }
     );
-
-    const getUsername = (userUID) => {
-        const q = query(collection(db, 'Users'), where('userUID', '==', userUID))
-        console.log(q)
-        const docSnap = getDocs(q);
-        console.log(docSnap)
-        //return docSnap[0].data().username
-    }
 
     return (
         <div>
@@ -42,12 +33,14 @@ const GiveFeedback = () => {
                     {error && <strong>Error! <br /> {JSON.stringify(error)}</strong>}
                     {loading && <em>Loading...</em>}
                     {posts && posts.docs.map((p) => {
+                        //var myUsername = String.toString(getUsername(p.data().author))
+                        var myUsername = 'jeff'
                         return (
                             <li key={p.id} className='flex flex-row p-4 m-4 ml-6 text-black bg-white'>
                                 <div>
                                     <h2 className="text-2xl font-medium font-HindSiliguri">{p.data().title}</h2>
                                     <div className="text-gray-500 font-IBMPlexSans">
-                                        <h4 className="text-sm text-[#808080]">Submitted by <Link href={`/profile/${p.data().author}`}><a className="underline">{p.data().author}</a></Link></h4>
+                                        <h4 className="text-sm text-[#808080]">Submitted by <Link href={`/profile/${p.data().author}`}><a className="underline">{myUsername}</a></Link></h4>
                                         <h4 className="text-sm"><em>Submitted on {p.data().created.toDate().toLocaleDateString('en-us', { weekday: "long", year: "numeric", month: "short", day: "numeric" })} {p.data().created.toDate().toLocaleTimeString('en-US', { hour: "2-digit", minute: "2-digit" })}</em></h4>
                                     </div>
                                     <br />
