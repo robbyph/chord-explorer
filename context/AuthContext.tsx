3766180f337a7a1faeb3a4c6a87698ae5de861f3
Robby <buckdestructor@gmail.com>
2023-02-23 13:40:05 -0500

    Made it so user data has the same UID as their auth UID
    This will solve many issues

--- context/AuthContext.tsx
+++ context/AuthContext.tsx
@@ -7,7 +7,7 @@ import {
 } from "firebase/auth";
 import { auth, db } from "../firebase/firestore";
 import { useCollection } from "react-firebase-hooks/firestore";
-import { collection, DocumentData, getDocs, orderBy, query, QueryDocumentSnapshot, where } from "firebase/firestore";
+import { collection, doc, DocumentData, getDocs, orderBy, query, QueryDocumentSnapshot, setDoc, where } from "firebase/firestore";
 
 interface UserType {
     email: string | null;
@@ -45,38 +45,39 @@ export const AuthContextProvider = ({ children }: { children: React.ReactNode })
     );
 
     useEffect(() => {
-        var acctData: DocumentData;
-        var commentData: QueryDocumentSnapshot<DocumentData>[]
-        var postsData: QueryDocumentSnapshot<DocumentData>[]
+        // var acctData: DocumentData;
+        // var commentData: QueryDocumentSnapshot<DocumentData>[]
+        // var postsData: QueryDocumentSnapshot<DocumentData>[]
 
-        if (account && account.docs[0] && account != undefined && account != null) {
-            acctData = account.docs[0].data()
-        } else {
-            acctData = { username: 'Account', bio: 'null' }
-        }
+        // console.log(account)
 
-        if (comments && comments.docs && comments != undefined && comments != null) {
-            commentData = comments.docs
-        } else {
-            commentData = []
-        }
+        // if (account && account.docs[0] && account != undefined && account != null) {
+        //     acctData = account.docs[0].data()
+        // } else {
+        //     acctData = { username: 'Account', bio: 'null' }
+        // }
 
-        if (posts && posts.docs && posts != undefined && posts != null) {
-            postsData = posts.docs
-        } else {
-            postsData = []
-        }
+        // if (comments && comments.docs && comments != undefined && comments != null) {
+        //     commentData = comments.docs
+        // } else {
+        //     commentData = []
+        // }
 
+        // if (posts && posts.docs && posts != undefined && posts != null) {
+        //     postsData = posts.docs
+        // } else {
+        //     postsData = []
+        // }
 
         const unsubscribe = onAuthStateChanged(auth, (user) => {
             if (user) {
                 setUser({
                     email: user.email,
                     uid: user.uid,
-                    username: acctData.username,
-                    bio: acctData.bio,
-                    comments: commentData,
-                    posts: postsData
+                    username: user.username,
+                    bio: user.bio,
+                    comments: user.comments,
+                    posts: user.posts
                 });
             } else {
                 setUser({ email: null, uid: null, username: null, bio: null, comments: null, posts: null });
@@ -87,12 +88,31 @@ export const AuthContextProvider = ({ children }: { children: React.ReactNode })
         return () => unsubscribe();
     }, [account, comments, posts]);
 
-    const signUp = async (email: string, password: string) => {
-        await createUserWithEmailAndPassword(auth, email, password);
+    const signUp = async (email: string, password: string, username: string) => {
+        //first we let firebase create their auth account
+        await createUserWithEmailAndPassword(auth, email, password).then((credentials) => {
+            setDoc(doc(db, "Users", credentials.user.uid), {
+                acctActive: true,
+                email: email,
+                username: username,
+                bio: "This user hasn't created their bio yet...",
+                comments: [],
+                posts: []
+            });
+        }).catch((error) => {
+            console.log(error.code)
+            if (error.code == "auth/email-already-in-use") { console.log("User already exists. Try to log in") }
+            else if (error.code == "auth/weak-password") { console.log("Password too weak. Strengthen it by increasing its length or complexity.") }
+
+        })
+        //then, we create a database object, with the same uid, for their other data
     };
 
     const logIn = async (email: string, password: string) => {
-        await signInWithEmailAndPassword(auth, email, password);
+        await signInWithEmailAndPassword(auth, email, password).catch((error) => {
+            console.log(error.code)
+            if (error.code == "auth/wrong-password") { console.log("Invalid Login Attempt") }
+        });
     };
 
     const logOut = async () => {
