9f2e6b8cb67225ba1c6daf7fddff155253f26f3c
Robby <buckdestructor@gmail.com>
2023-03-09 12:23:16 -0500

    Chord Recognition Tabs

--- pages/chordrecognition.tsx
+++ pages/chordrecognition.tsx
@@ -4,7 +4,8 @@ import React, { useState, useRef } from "react";
 import Head from "next/head";
 import { detectChords } from "../akkorder/src/index";
 import { silenceRemovalAlgorithm } from "../utilities/remove_silence";
-
+import { Tab } from '@headlessui/react'
+import { Fragment } from 'react'
 
 const ChordRecognition = () => {
 
@@ -137,21 +138,52 @@ const ChordRecognition = () => {
             <main className="p-6 m-2">
                 <h1 className="col-span-4 text-4xl font-semibold font-HindSiliguri">Chord Recognition</h1>
                 <h2 className="col-span-4 pb-4 text-2xl font-HindSiliguri">Powered by AI</h2>
-                <div className="col-span-4 p-2 pl-0 m-2 mb-8 ml-0 ">
-                    <h2 className="col-span-4 pt-2 mb-2 text-2xl font-HindSiliguri">First, Upload an MP3 or WAV Audio File</h2>
-                    {submitWarning ? <p className="font-bold text-red-500"> Please upload an audio file before generating chord data...</p> : ''}
-                    {recognitionWarning ? <p className="font-bold text-red-500">Please upload an audio file before generating chord data...</p> : ''}
-                    {submittedIndicator ? <p className="font-bold text-white">Uploaded!</p> : ''}
-                    <input type="file" name="myImage" className="my-2" onChange={uploadToClient} /> <br />
-                </div>
-                <h2 className="col-span-4 pt-2 mb-2 text-2xl font-HindSiliguri">Then, let the AI do the rest</h2>
-                <button className="col-span-4 px-2 py-2 text-xl border font-IBMPlexSans font-medium bg-white text-[#5B21B6] " onClick={() => uploadToServer()}>Detect Chords</button>
-                <h3 className="col-span-4 pt-2 text-xl underline font-HindSiliguri">Chords Detected</h3>
-                <ul className="list-disc list-inside font-IBMPlexSans">
-                    {detectedChords.map((chord, i) => {
-                        return <li key={i}>{chord.rootNote} {toTextQuality(chord.quality)} {chord.interval != 0 ? chord.interval : ''}</li>
-                    })}
-                </ul>
+
+                <Tab.Group>
+                    <Tab.List>
+                        <Tab as={Fragment}>
+                            {({ selected }) => (
+                                /* Use the `selected` state to conditionally style the selected tab. */
+                                <button
+                                    className={
+                                        `p-2 border xl:mr-2 ${selected ? 'bg-white text-black' : 'bg-opacity-0 text-white'}`
+                                    }
+                                >
+                                    Live Chord Recognition
+                                </button>
+                            )}</Tab>
+                        <Tab as={Fragment}>
+                            {({ selected }) => (
+                                /* Use the `selected` state to conditionally style the selected tab. */
+                                <button
+                                    className={
+                                        `p-2 border xl:mr-2 ${selected ? 'bg-white text-purple-600' : 'bg-opacity-0 text-white'}`
+                                    }
+                                >
+                                    Chord Recognition from a File
+                                </button>
+                            )}</Tab>
+                    </Tab.List>
+                    <Tab.Panels className='pt-0 mt-2'>
+                        <Tab.Panel>Content 1</Tab.Panel>
+                        <Tab.Panel><div className="col-span-4 p-2 pl-0 m-2 mb-8 ml-0 ">
+                            <h2 className="col-span-4 pt-2 mb-2 text-2xl font-HindSiliguri">First, Upload an MP3 or WAV Audio File</h2>
+                            {submitWarning ? <p className="font-bold text-red-500"> Please upload an audio file before generating chord data...</p> : ''}
+                            {recognitionWarning ? <p className="font-bold text-red-500">Please upload an audio file before generating chord data...</p> : ''}
+                            {submittedIndicator ? <p className="font-bold text-white">Uploaded!</p> : ''}
+                            <input type="file" name="myImage" className="my-2" onChange={uploadToClient} /> <br />
+                        </div>
+                            <h2 className="col-span-4 pt-2 mb-2 text-2xl font-HindSiliguri">Then, let the AI do the rest</h2>
+                            <button className="col-span-4 px-2 py-2 text-xl border font-IBMPlexSans font-medium bg-white text-[#5B21B6]" onClick={() => uploadToServer()}>Detect Chords</button>
+                            <h3 className="col-span-4 pt-2 text-xl underline font-HindSiliguri">Chords Detected</h3>
+                            <ul className="list-disc list-inside font-IBMPlexSans">
+                                {detectedChords.map((chord, i) => {
+                                    return <li key={i}>{chord.rootNote} {toTextQuality(chord.quality)} {chord.interval != 0 ? chord.interval : ''}</li>
+                                })}
+                            </ul></Tab.Panel>
+                    </Tab.Panels>
+                </Tab.Group>
+
             </main>
         </div>
     );
