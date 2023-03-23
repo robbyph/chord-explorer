f1c58473770dd88ff91184b67442695321891162
Robby <buckdestructor@gmail.com>
2023-03-23 15:55:18 -0400

    Overhauled comments and comment voting

--- pages/post/[id].tsx
+++ pages/post/[id].tsx
@@ -26,6 +26,7 @@ const PostPage = (props) => {
 
     const [newComment, setNewComment] = useState('');
     const [showAlert, setShowAlert] = useState(false)
+    const [showVotingAlert, setShowVotingAlert] = useState(false)
     const [showSignInPrompt, setShowSignInPrompt] = useState(false)
     const [comments, loading, error] = useCollection(
         query(collection(db, 'Comments'), where('parentPost', '==', props.id), orderBy('created', 'desc')),
@@ -68,7 +69,9 @@ const PostPage = (props) => {
                 authorUserName: loggedInUserData?.data().username,
                 helpfulCount: 0,
                 unhelpfulCount: 0,
-                parentPost: props.id
+                parentPost: props.id,
+                upvoteUsers: [],
+                downvoteUsers: [],
             });
         };
 
@@ -79,9 +82,43 @@ const PostPage = (props) => {
         const upvoteRef = doc(db, 'Comments', c.id)
         const upvoteCount = c.data().helpfulCount + 1;
 
+        //determine if the user is the comment author
+        if (c.data().author === loggedInUserData?.id) {
+            setShowVotingAlert(true)
+            console.log('cannot vote own comment')
+            return;
+        }
+
+        //determine if user has already upvoted
+        if (c.data().upvoteUsers.includes(loggedInUserData?.id)) {
+            console.log('already upvoted')
+            //remove the user from the list of upvoters
+            const upvoteUsers = c.data().upvoteUsers.filter((user) => user !== loggedInUserData?.id)
+            return updateDoc(upvoteRef, {
+                updated: serverTimestamp(),
+                helpfulCount: upvoteCount - 2,
+                upvoteUsers: upvoteUsers
+            });
+        }
+
+        //determine if user has already downvoted
+        if (c.data().downvoteUsers.includes(loggedInUserData?.id)) {
+            console.log('already downvoted')
+            //remove the user from the list of downvoters
+            const downvoteUsers = c.data().downvoteUsers.filter((user) => user !== loggedInUserData?.id)
+            return updateDoc(upvoteRef, {
+                updated: serverTimestamp(),
+                helpfulCount: upvoteCount,
+                unhelpfulCount: c.data().unhelpfulCount - 1,
+                upvoteUsers: [...c.data().upvoteUsers, loggedInUserData?.id],
+                downvoteUsers: downvoteUsers
+            });
+        }
+
         return updateDoc(upvoteRef, {
             updated: serverTimestamp(),
             helpfulCount: upvoteCount,
+            upvoteUsers: [...c.data().upvoteUsers, loggedInUserData?.id]
         });
     };
 
@@ -89,9 +126,43 @@ const PostPage = (props) => {
         const downvoteRef = doc(db, 'Comments', c.id)
         const downvoteCount = c.data().unhelpfulCount + 1;
 
+        //determine if the user is the comment author
+        if (c.data().author === loggedInUserData?.id) {
+            console.log('cannot vote own comment')
+            setShowVotingAlert(true)
+            return;
+        }
+
+        //determine if user has already downvoted
+        if (c.data().downvoteUsers.includes(loggedInUserData?.id)) {
+            console.log('already downvoted')
+            //remove the user from the list of upvoters
+            const downvoteUsers = c.data().downvoteUsers.filter((user) => user !== loggedInUserData?.id)
+            return updateDoc(downvoteRef, {
+                updated: serverTimestamp(),
+                unhelpfulCount: downvoteCount - 2,
+                downvoteUsers: downvoteUsers
+            });
+        }
+
+        //determine if the user has already upvoted
+        if (c.data().upvoteUsers.includes(loggedInUserData?.id)) {
+            console.log('already upvoted')
+            //remove the user from the list of upvoters
+            const upvoteUsers = c.data().upvoteUsers.filter((user) => user !== loggedInUserData?.id)
+            return updateDoc(downvoteRef, {
+                updated: serverTimestamp(),
+                helpfulCount: c.data().helpfulCount - 1,
+                unhelpfulCount: downvoteCount,
+                upvoteUsers: upvoteUsers,
+                downvoteUsers: [...c.data().downvoteUsers, loggedInUserData?.id]
+            });
+        }
+
         return updateDoc(downvoteRef, {
             updated: serverTimestamp(),
             unhelpfulCount: downvoteCount,
+            downvoteUsers: [...c.data().downvoteUsers, loggedInUserData?.id]
         });
     };
 
@@ -107,6 +178,7 @@ const PostPage = (props) => {
     return (
         <div id='page'>
             {showAlert && <Alert message="⚠️ Profanity Warning! ⚠️" setShow={setShowAlert} />}
+            {showVotingAlert && <Alert message='You cannot vote on your own comment!' setShow={setShowVotingAlert} />}
             {showSignInPrompt && <SignInPrompt setShow={setShowSignInPrompt} />}
             <div className='flex flex-col items-center w-1/2 mx-auto mt-10 space-y-6' >
                 <div id='header' className='flex flex-col items-center pb-6 text-center'>
@@ -144,7 +216,7 @@ const PostPage = (props) => {
                                     <div className='flex flex-row items-end space-x-4 font-HindSiliguri'>
                                         <div className='mr-auto space-x-4'>
                                             <button onClick={() => handleUpvote(c)} className='px-2 py-1 font-medium text-black transition bg-purple-200 border border-purple-400 shadow-md active:-translate-y-1'>{c.data().helpfulCount} | Helpful</button>
-                                            <button onClick={() => handleDownvote(c)} className='px-2 py-1 font-medium text-black bg-purple-200 border border-purple-400 shadow-md active:-translate-y-1'>{c.data().unhelpfulCount} | Unhelpful</button>
+                                            <button onClick={() => handleDownvote(c)} className='px-2 py-1 font-medium text-black transition bg-purple-200 border border-purple-400 shadow-md active:-translate-y-1'>{c.data().unhelpfulCount} | Unhelpful</button>
                                         </div>
                                         <div>
                                             <h4 id='comment-author' className='pt-4 pb-0 text-lg font-medium font-HindSiliguri'>From <Link href={`/profile/${c.data().author}`}><a className="underline">{c.data().authorUserName}</a></Link></h4>
