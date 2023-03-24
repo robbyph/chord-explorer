66d9d3103631a83b5f8ef1d1fe5c86f616780478
Robby <buckdestructor@gmail.com>
2023-03-24 17:45:50 -0400

    Chord Library Dummy Data

--- pages/chordlibrary.tsx
+++ pages/chordlibrary.tsx
@@ -1,7 +1,22 @@
 import React from 'react'
 import Head from 'next/head'
+import { useState } from "react";
+import ChordModal from '../components/ChordModal';
+
+//dummy song data
+const chords = ['Major', 'Minor', 'Diminished', 'Sus2', 'Sus4']
+
+//song modal component
 
 const ChordLibrary = () => {
+    const [selectedChord, setSelectedChord] = useState({});
+    const [modalOpen, setModalOpen] = useState(false);
+    const [chordRoot, setChordRoot] = useState('C');
+
+    const handleChange = (event) => {
+        setChordRoot(event.target.value);
+    };
+
     return (
         <div>
             <Head>
@@ -12,12 +27,16 @@ const ChordLibrary = () => {
                 />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <main >
+            <main>
+                {modalOpen &&
+                    <ChordModal
+                        chord={selectedChord}
+                        onClose={() => setModalOpen(false)}
+                    />}
                 <h1 className="col-span-12 p-6 text-4xl font-semibold font-HindSiliguri">Chord Library</h1>
                 <div className="flex flex-col w-1/6 px-4 mb-4" id="parameters">
                     <label className="mb-1 ml-1 font-semibold" htmlFor="chordRoot">Chord Root</label>
-                    <select name="chordRoot" id="chordRootSelect" className="h-10 px-3 border-gray-300 rounded">
-                        <option value="any">Any</option>
+                    <select value={chordRoot} name="chordRoot" id="chordRootSelect" className="h-10 px-3 border-gray-300 rounded" onChange={handleChange}>
                         <option value="C">C</option>
                         <option value="C#">C#/Db</option>
                         <option value="D">D</option>
@@ -36,34 +55,17 @@ const ChordLibrary = () => {
                     Chords
                 </h2>
                 <div className="grid grid-cols-4 gap-4 px-4 justify-items-stretch">
-                    <div className="flex flex-col p-4 text-center text-black bg-white rounded">
-                        <h3 className="pb-2 text-3xl font-semibold font-HindSiliguri">A Major</h3>
-                        <img className='w-40 mx-auto' src='https://placehold.jp/150x150.png' alt='' />
-                        <button className={`p-2 mt-4 px-8 m-2 ml-0 bg-white border-2 text-lg cursor-pointer text-[#5B21B6] font-IBMPlexSans font-medium hover:text-white hover:bg-[#5B21B6]`}>Read More & Listen</button>
-                    </div>
-                    <div className="flex flex-col p-4 text-center text-black bg-white rounded">
-                        <h3 className="pb-2 text-3xl font-semibold font-HindSiliguri">A Major</h3>
-                        <img className='w-40 mx-auto' src='https://placehold.jp/150x150.png' alt='' />
-                        <button className={`p-2 mt-4 px-8 m-2 ml-0 bg-white border-2 text-lg cursor-pointer text-[#5B21B6] font-IBMPlexSans font-medium hover:text-white hover:bg-[#5B21B6]`}>Read More & Listen</button>
-                    </div>
-                    <div className="flex flex-col p-4 text-center text-black bg-white rounded">
-                        <h3 className="pb-2 text-3xl font-semibold font-HindSiliguri">A Major</h3>
-                        <img className='w-40 mx-auto' src='https://placehold.jp/150x150.png' alt='' />
-                        <button className={`p-2 mt-4 px-8 m-2 ml-0 bg-white border-2 text-lg cursor-pointer text-[#5B21B6] font-IBMPlexSans font-medium hover:text-white hover:bg-[#5B21B6]`}>Read More & Listen</button>
-                    </div>
-                    <div className="flex flex-col p-4 text-center text-black bg-white rounded">
-                        <h3 className="pb-2 text-3xl font-semibold font-HindSiliguri">A Major</h3>
-                        <img className='w-40 mx-auto' src='https://placehold.jp/150x150.png' alt='' />
-                        <button className={`p-2 mt-4 px-8 m-2 ml-0 bg-white border-2 text-lg cursor-pointer text-[#5B21B6] font-IBMPlexSans font-medium hover:text-white hover:bg-[#5B21B6]`}>Read More & Listen</button>
-                    </div>
-                    <div className="flex flex-col p-4 text-center text-black bg-white rounded">
-                        <h3 className="pb-2 text-3xl font-semibold font-HindSiliguri">A Major</h3>
-                        <img className='w-40 mx-auto' src='https://placehold.jp/150x150.png' alt='' />
-                        <button className={`p-2 mt-4 px-8 m-2 ml-0 bg-white border-2 text-lg cursor-pointer text-[#5B21B6] font-IBMPlexSans font-medium hover:text-white hover:bg-[#5B21B6]`}>Read More & Listen</button>
-                    </div>
-
+                    {chords.map((chord) =>
+                        <div key={chord} className="flex flex-col p-4 text-center text-black bg-white rounded">
+                            <h3 className="pb-2 text-3xl font-semibold font-HindSiliguri">{chordRoot} {chord}</h3>
+                            <img className='w-40 mx-auto' src='https://placehold.jp/150x150.png' alt='' />
+                            <button onClick={() => {
+                                setSelectedChord(chord);
+                                setModalOpen(true);
+                            }} className={`p-2 mt-4 px-8 m-2 ml-0 bg-white border-2 text-lg cursor-pointer text-[#5B21B6] font-IBMPlexSans font-medium hover:text-white hover:bg-[#5B21B6]`}>Read More & Listen</button>
+                        </div>
+                    )}
                 </div>
-
             </main>
         </div>
     )
