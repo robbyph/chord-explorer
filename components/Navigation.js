a9a064e21351e5c3f17b066de5bdb64d032deff8
Robby <buckdestructor@gmail.com>
2023-04-03 23:35:03 -0400

    Fixed nav

--- components/Navigation.js
+++ components/Navigation.js
@@ -38,28 +38,96 @@ const Navigation = ({ currentPage }) => {
     <>
       <nav className='z-10 flex flex-row gap-8 p-4 text-black font-IBMPlexSans'>
         <Link href='/'>
-          <a onClick={() => setShow(false)}>Home</a>
+          <a
+            onClick={() => setShow(false)}
+            className='hover:animate-bounceslowonce'
+          >
+            Home
+          </a>
         </Link>
         <Link href='/search'>
-          <a onClick={() => setShow(false)}>Search</a>
+          <a
+            onClick={() => setShow(false)}
+            className='hover:animate-bounceslowonce'
+          >
+            Search
+          </a>
         </Link>
         <Link href='/chordlibrary'>
-          <a onClick={() => setShow(false)}>Chord Library</a>
+          <a
+            onClick={() => setShow(false)}
+            className='hover:animate-bounceslowonce'
+          >
+            Chord Library
+          </a>
         </Link>
         <Link href='/givefeedback'>
-          <a onClick={() => setShow(false)}>Give Feedback</a>
+          <a
+            onClick={() => setShow(false)}
+            className='hover:animate-bounceslowonce'
+          >
+            Give Feedback
+          </a>
         </Link>
         <Link href='/submitfeedback'>
-          <a onClick={() => setShow(false)}>Request Feedback</a>
+          <a
+            onClick={() => setShow(false)}
+            className='hover:animate-bounceslowonce'
+          >
+            Submit for Feedback
+          </a>
         </Link>
-        <Link href='/accountpage'>
-          <a onClick={() => setShow(false)}>Account</a>
-        </Link>
+        <Menu as='div' className='hover:animate-bounceslowonce'>
+          <Menu.Button>
+            {username}
+            <span className='text-xs'> ▼</span>
+          </Menu.Button>
+          <Menu.Items className='absolute flex flex-col bg-purple-200 border-2 border-purple-500 divide-y divide-white'>
+            <Menu.Item className='flex-col'>
+              {({ active }) => (
+                <div className='px-2 py-1 hover:bg-purple-600 hover:text-white'>
+                  <Link href='/accountpage'>
+                    <a
+                      className={`${active && 'font-HindSiliguri'}`}
+                      onClick={() => setShow(false)}
+                    >
+                      View Account
+                    </a>
+                  </Link>
+                </div>
+              )}
+            </Menu.Item>
+            <Menu.Item className='flex-col'>
+              {({ active }) => (
+                <div className='px-2 py-1 hover:bg-purple-600 hover:text-white'>
+                  <Link href='/'>
+                    <a
+                      onClick={handleLogout}
+                      className={`${active && 'font-HindSiliguri'}`}
+                    >
+                      Log Out
+                    </a>
+                  </Link>
+                </div>
+              )}
+            </Menu.Item>
+          </Menu.Items>
+        </Menu>
         <Link href='/submitsong'>
-          <a onClick={() => setShow(false)}>Submit A Song</a>
+          <a
+            onClick={() => setShow(false)}
+            className='hover:animate-bounceslowonce'
+          >
+            Submit A Song
+          </a>
         </Link>
         <Link href='/chordrecognition'>
-          <a onClick={() => setShow(false)}>Chord Recognition</a>
+          <a
+            onClick={() => setShow(false)}
+            className='hover:animate-bounceslowonce'
+          >
+            Chord Recognition
+          </a>
         </Link>
       </nav>
     </>
