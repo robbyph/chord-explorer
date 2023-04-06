b57766f1939cae2af89db0681aca49f9d8695ed2
Andrew Glanz <andrewvita0@gmail.com>
2023-04-06 00:28:44 -0400

    Changed wording on search page to match content.

--- pages/search.tsx
+++ pages/search.tsx
@@ -2,191 +2,240 @@ import React, { useEffect } from "react";
 import Head from "next/head";
 import SongModal from "../components/SongModal";
 import { useCollection } from "react-firebase-hooks/firestore";
-import { collection, CollectionReference, DocumentData, FirestoreError, onSnapshot, orderBy, query, where } from "firebase/firestore";
+import {
+  collection,
+  CollectionReference,
+  DocumentData,
+  FirestoreError,
+  onSnapshot,
+  orderBy,
+  query,
+  where,
+} from "firebase/firestore";
 import { db } from "../firebase/firestore";
 import { useState } from "react";
 
 const Search = () => {
-    const [selectedSong, setSelectedSong] = useState({});
-    const [modalOpen, setModalOpen] = useState(false);
-    const [chordRoot, setChordRoot] = useState("any");
-    const [chordQuality, setChordQuality] = useState("any");
-    const [genre, setGenre] = useState("any");
-    const [difficulty, setDifficulty] = useState("any");
+  const [selectedSong, setSelectedSong] = useState({});
+  const [modalOpen, setModalOpen] = useState(false);
+  const [chordRoot, setChordRoot] = useState("any");
+  const [chordQuality, setChordQuality] = useState("any");
+  const [genre, setGenre] = useState("any");
+  const [difficulty, setDifficulty] = useState("any");
 
-    const [firebaseSongs, setFirebaseSongs] = useState<{ data: any; id: string }[]>([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState<null | FirestoreError>(null);
+  const [firebaseSongs, setFirebaseSongs] = useState<
+    { data: any; id: string }[]
+  >([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<null | FirestoreError>(null);
 
-    // Define default query
-    let q = query(collection(db, 'Songs'));
+  // Define default query
+  let q = query(collection(db, "Songs"));
 
-    useEffect(() => {
-        // Modify query whenever any of the select elements change
+  useEffect(() => {
+    // Modify query whenever any of the select elements change
 
-        console.log(chordRoot, chordQuality, genre, difficulty)
+    console.log(chordRoot, chordQuality, genre, difficulty);
 
-        if (chordRoot == 'any' && chordQuality != 'any') {
-            q = query(q,
-                where('chords', '>=', chordQuality),
-                where('chords', '<=', chordQuality + '\uf8ff'),
-                orderBy('chords')
-            );
-        } else if (chordRoot != 'any' && chordQuality == 'any') {
-            q = query(q,
-                where('chords', '>=', chordRoot),
-                where('chords', '<=', chordRoot + '\uf8ff'),
-                orderBy('chords')
-            );
-        }
+    if (chordRoot == "any" && chordQuality != "any") {
+      q = query(
+        q,
+        where("chords", ">=", chordQuality),
+        where("chords", "<=", chordQuality + "\uf8ff"),
+        orderBy("chords")
+      );
+    } else if (chordRoot != "any" && chordQuality == "any") {
+      q = query(
+        q,
+        where("chords", ">=", chordRoot),
+        where("chords", "<=", chordRoot + "\uf8ff"),
+        orderBy("chords")
+      );
+    }
 
-        if (chordRoot != 'any' && chordQuality != 'any') {
-            var chord = `${chordRoot} ${chordQuality}`;
-            q = query(q, where('chords', 'array-contains', chord));
-        }
+    if (chordRoot != "any" && chordQuality != "any") {
+      var chord = `${chordRoot} ${chordQuality}`;
+      q = query(q, where("chords", "array-contains", chord));
+    }
 
-        if (genre != 'any') {
-            q = query(q, where('genre', '==', genre));
-        }
+    if (genre != "any") {
+      q = query(q, where("genre", "==", genre));
+    }
 
-        if (difficulty != 'any') {
-            q = query(q, where('difficulty', '==', difficulty));
-        }
+    if (difficulty != "any") {
+      q = query(q, where("difficulty", "==", difficulty));
+    }
 
-        q = query(q, orderBy('title'));
+    q = query(q, orderBy("title"));
 
-        const unsubscribe = onSnapshot(q, (querySnapshot) => {
-            const songs: { data: any; id: string }[] = [];
-            querySnapshot.forEach((doc) => {
-                songs.push({ data: doc.data(), id: doc.id });
-            });
-            console.log(songs)
-            setFirebaseSongs(songs);
-            setLoading(false);
-        });
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      const songs: { data: any; id: string }[] = [];
+      querySnapshot.forEach((doc) => {
+        songs.push({ data: doc.data(), id: doc.id });
+      });
+      console.log(songs);
+      setFirebaseSongs(songs);
+      setLoading(false);
+    });
 
+    return () => unsubscribe();
+  }, [chordRoot, chordQuality, genre, difficulty]);
 
-
-        return () => unsubscribe();
-    }, [chordRoot, chordQuality, genre, difficulty]);
-
-    return (
-        <div>
-            <Head>
-                <title>Chord Explorer</title>
-                <meta
-                    name="description"
-                    content="A music education website where you can search for songs based on the chords you're learning/teaching, as well as getting community based feedback on your playing!"
-                />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-            {loading && <h1>Loading...</h1>}
-            {error && <strong>Error! <br /> {JSON.stringify(error)}</strong>}
-            {
-                firebaseSongs && <main>
-                    {modalOpen && <SongModal song={selectedSong}
-                        onClose={() => setModalOpen(false)}
-                    />}
-                    <h1 className="col-span-4 p-6 text-4xl font-semibold font-HindSiliguri">
-                        Search
-                    </h1>
-                    <div className="grid grid-cols-4 gap-4 px-4">
-                        <div className="flex flex-col mb-4">
-                            <label className="mb-1 ml-1 font-semibold" htmlFor="chordRoot">Chord Root</label>
-                            <select value={chordRoot} onChange={
-                                (e) => {
-                                    setChordRoot(e.target.value);
-                                }
-                            } name="chordRoot" id="chordRootSelect" className="h-10 px-3 border-gray-300 rounded">
-                                <option value="any">Any</option>
-                                <option value="C">C</option>
-                                <option value="C#">C#/Db</option>
-                                <option value="D">D</option>
-                                <option value="D#">D#/Eb</option>
-                                <option value="E">E</option>
-                                <option value="F">F</option>
-                                <option value="F#">F#/Gb</option>
-                                <option value="G">G</option>
-                                <option value="G#">G#/Ab</option>
-                                <option value="A">A</option>
-                                <option value="A#">A#/Bb</option>
-                                <option value="B">B</option>
-                            </select>
-                        </div>
-                        <div className="flex flex-col mb-4">
-                            <label className="mb-1 ml-1 font-semibold" htmlFor="chordQuality">Chord Quality</label>
-                            <select name="chordQuality" value={chordQuality} onChange={
-                                (e) => {
-                                    setChordQuality(e.target.value);
-                                }
-                            } id="chordQualitySelect" className="h-10 px-3 border-gray-300 rounded">
-                                <option value="any">Any</option>
-                                <option value="major">Major</option>
-                                <option value="minor">Minor</option>
-                                <option value="dim">Diminished</option>
-                                <option value="aug">Augmented</option>
-                                <option value="sus4">Sus2</option>
-                                <option value="sus4">Sus4</option>
-                                <option value="maj7">Major 7</option>
-                                <option value="m7">Minor 7</option>
-                                <option value="7">Dominant 7</option>
-                            </select>
-                        </div>
-                        <div className="flex flex-col mb-4">
-                            <label className="mb-1 ml-1 font-semibold" htmlFor="genre">Genre</label>
-                            <select name="genre" value={genre} onChange={
-                                (e) => {
-                                    setGenre(e.target.value);
-                                }
-                            } id="genre" className="h-10 px-3 border-gray-300 rounded">
-                                <option value="any">Any</option>
-                                <option value="rock">Rock</option>
-                                <option value="jazz">Jazz</option>
-                                <option value="pop">Pop</option>
-                                <option value="folk">Folk</option>
-                                <option value="indie">Indie</option>
-                            </select>
-                        </div>
-                        <div className="flex flex-col mb-4">
-                            <label className="mb-1 ml-1 font-semibold" htmlFor="difficulty">Difficulty</label>
-                            <select value={difficulty} onChange={
-                                (e) => {
-                                    setDifficulty(e.target.value);
-                                }
-                            } name="difficulty" id="chordQualitySelect" className="h-10 px-3 border-gray-300 rounded">
-                                <option value="any">Any</option>
-                                <option value="easy">Easy</option>
-                                <option value="intermediate">Intermediate</option>
-                                <option value="hard">Hard</option>
-                            </select>
-                        </div>
-                    </div>
-                    <h2 className="col-span-4 p-6 text-3xl font-semibold font-HindSiliguri">
-                        Songs Containing G Major (G)
-                    </h2>
-                    <div className="grid grid-cols-4 gap-4 px-4 justify-items-stretch">
-                        {firebaseSongs.map((song) => {
-                            return (
-                                <div key={song.id} className="flex flex-col justify-center p-4 text-black bg-white rounded">
-                                    <h3 className="text-2xl font-semibold font-HindSiliguri">{song.data.title}</h3>
-                                    <p className="text-lg font-HindSiliguri">By {song.data.artist}</p>
-                                    <button
-                                        onClick={() => {
-                                            setSelectedSong(song);
-                                            setModalOpen(true);
-                                        }}
-                                        className={`p-2 px-8 m-2 ml-0 bg-white border-2 text-lg cursor-pointer text-[#5B21B6] font-IBMPlexSans font-medium hover:text-white hover:bg-[#5B21B6]`}>View Chord Charts</button>
-                                </div>
-                            )
-                        })}
-                    </div>
-                </main >
-            }
-        </div >
-    );
+  return (
+    <div>
+      <Head>
+        <title>Chord Explorer</title>
+        <meta
+          name="description"
+          content="A music education website where you can search for songs based on the chords you're learning/teaching, as well as getting community based feedback on your playing!"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      {loading && <h1>Loading...</h1>}
+      {error && (
+        <strong>
+          Error! <br /> {JSON.stringify(error)}
+        </strong>
+      )}
+      {firebaseSongs && (
+        <main>
+          {modalOpen && (
+            <SongModal
+              song={selectedSong}
+              onClose={() => setModalOpen(false)}
+            />
+          )}
+          <h1 className="col-span-4 p-6 text-4xl font-semibold font-HindSiliguri">
+            Search
+          </h1>
+          <div className="grid grid-cols-4 gap-4 px-4">
+            <div className="flex flex-col mb-4">
+              <label className="mb-1 ml-1 font-semibold" htmlFor="chordRoot">
+                Chord Root
+              </label>
+              <select
+                value={chordRoot}
+                onChange={(e) => {
+                  setChordRoot(e.target.value);
+                }}
+                name="chordRoot"
+                id="chordRootSelect"
+                className="h-10 px-3 border-gray-300 rounded"
+              >
+                <option value="any">Any</option>
+                <option value="C">C</option>
+                <option value="C#">C#/Db</option>
+                <option value="D">D</option>
+                <option value="D#">D#/Eb</option>
+                <option value="E">E</option>
+                <option value="F">F</option>
+                <option value="F#">F#/Gb</option>
+                <option value="G">G</option>
+                <option value="G#">G#/Ab</option>
+                <option value="A">A</option>
+                <option value="A#">A#/Bb</option>
+                <option value="B">B</option>
+              </select>
+            </div>
+            <div className="flex flex-col mb-4">
+              <label className="mb-1 ml-1 font-semibold" htmlFor="chordQuality">
+                Chord Quality
+              </label>
+              <select
+                name="chordQuality"
+                value={chordQuality}
+                onChange={(e) => {
+                  setChordQuality(e.target.value);
+                }}
+                id="chordQualitySelect"
+                className="h-10 px-3 border-gray-300 rounded"
+              >
+                <option value="any">Any</option>
+                <option value="major">Major</option>
+                <option value="minor">Minor</option>
+                <option value="dim">Diminished</option>
+                <option value="aug">Augmented</option>
+                <option value="sus4">Sus2</option>
+                <option value="sus4">Sus4</option>
+                <option value="maj7">Major 7</option>
+                <option value="m7">Minor 7</option>
+                <option value="7">Dominant 7</option>
+              </select>
+            </div>
+            <div className="flex flex-col mb-4">
+              <label className="mb-1 ml-1 font-semibold" htmlFor="genre">
+                Genre
+              </label>
+              <select
+                name="genre"
+                value={genre}
+                onChange={(e) => {
+                  setGenre(e.target.value);
+                }}
+                id="genre"
+                className="h-10 px-3 border-gray-300 rounded"
+              >
+                <option value="any">Any</option>
+                <option value="rock">Rock</option>
+                <option value="jazz">Jazz</option>
+                <option value="pop">Pop</option>
+                <option value="folk">Folk</option>
+                <option value="indie">Indie</option>
+              </select>
+            </div>
+            <div className="flex flex-col mb-4">
+              <label className="mb-1 ml-1 font-semibold" htmlFor="difficulty">
+                Difficulty
+              </label>
+              <select
+                value={difficulty}
+                onChange={(e) => {
+                  setDifficulty(e.target.value);
+                }}
+                name="difficulty"
+                id="chordQualitySelect"
+                className="h-10 px-3 border-gray-300 rounded"
+              >
+                <option value="any">Any</option>
+                <option value="easy">Easy</option>
+                <option value="intermediate">Intermediate</option>
+                <option value="hard">Hard</option>
+              </select>
+            </div>
+          </div>
+          <h2 className="col-span-4 p-6 text-3xl font-semibold font-HindSiliguri">
+            Songs
+          </h2>
+          <div className="grid grid-cols-4 gap-4 px-4 justify-items-stretch">
+            {firebaseSongs.map((song) => {
+              return (
+                <div
+                  key={song.id}
+                  className="flex flex-col justify-center p-4 text-black bg-white rounded"
+                >
+                  <h3 className="text-2xl font-semibold font-HindSiliguri">
+                    {song.data.title}
+                  </h3>
+                  <p className="text-lg font-HindSiliguri">
+                    By {song.data.artist}
+                  </p>
+                  <button
+                    onClick={() => {
+                      setSelectedSong(song);
+                      setModalOpen(true);
+                    }}
+                    className={`p-2 px-8 m-2 ml-0 bg-white border-2 text-lg cursor-pointer text-[#5B21B6] font-IBMPlexSans font-medium hover:text-white hover:bg-[#5B21B6]`}
+                  >
+                    View Chord Charts
+                  </button>
+                </div>
+              );
+            })}
+          </div>
+        </main>
+      )}
+    </div>
+  );
 };
-
-
-
 
 export default Search;
