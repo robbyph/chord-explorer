903f998066fa3ce29c6fbc2d18e3e605b80301cd
Robby <buckdestructor@gmail.com>
2023-04-13 19:09:54 -0400

    Added logout button for mobile

--- components/hamburger.js
+++ components/hamburger.js
@@ -1,5 +1,7 @@
 import Link from 'next/link';
 import { slide as Menu } from 'react-burger-menu';
+import { useAuth } from '../context/AuthContext';
+
 const HamburgerMenu = () => (
   <div className='relative p-2 '>
     <Menu
@@ -29,33 +31,55 @@ const HamburgerIcon = () => (
   </div>
 );
 
-export const Links = () => (
-  <>
-    <Link href='/'>
-      <a className='p-4 font-bold'>Home</a>
-    </Link>
-    <Link href='/search'>
-      <a className='p-4 font-bold'>Search</a>
-    </Link>
-    <Link href='/chordlibrary'>
-      <a className='p-4 font-bold'>Chord Library</a>
-    </Link>
-    <Link href='/givefeedback'>
-      <a className='p-4 font-bold'>Give Feedback</a>
-    </Link>
-    <Link href='/submitfeedback'>
-      <a className='p-4 font-bold'>Request Feedback</a>
-    </Link>
-    <Link href='/accountpage'>
-      <a className='p-4 font-bold'>Account</a>
-    </Link>
-    <Link href='/submitsong'>
-      <a className='p-4 font-bold'>Submit A Song</a>
-    </Link>
-    <Link href='/chordrecognition'>
-      <a className='p-4 font-bold'>Chord Recognition</a>
-    </Link>
-  </>
-);
+export const Links = () => {
+  const { user, logOut } = useAuth();
+
+  const handleLogout = async () => {
+    try {
+      await logOut();
+      router.push('/login');
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  console.log(user);
+
+  return (
+    <>
+      <Link href='/'>
+        <a className='p-4 font-bold'>Home</a>
+      </Link>
+      <Link href='/search'>
+        <a className='p-4 font-bold'>Search</a>
+      </Link>
+      <Link href='/chordlibrary'>
+        <a className='p-4 font-bold'>Chord Library</a>
+      </Link>
+      <Link href='/givefeedback'>
+        <a className='p-4 font-bold'>Give Feedback</a>
+      </Link>
+      <Link href='/submitfeedback'>
+        <a className='p-4 font-bold'>Request Feedback</a>
+      </Link>
+      <Link href='/submitsong'>
+        <a className='p-4 font-bold'>Submit A Song</a>
+      </Link>
+      <Link href='/chordrecognition'>
+        <a className='p-4 font-bold'>Chord Recognition</a>
+      </Link>
+      <Link href='/accountpage'>
+        <a className='p-4 font-bold'>Account</a>
+      </Link>
+      {user.uid != null && (
+        <Link href='/login'>
+          <a onClick={handleLogout} className='p-4 font-bold'>
+            Log Out
+          </a>
+        </Link>
+      )}
+    </>
+  );
+};
 
 export default HamburgerMenu;
